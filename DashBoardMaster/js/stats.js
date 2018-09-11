a3b5db5584824a6703f22034f42622219d583a20
rababjamshed <rabab_imman@hotmail.com>
2018-09-11 15:50:47 +0500

    Add files via upload

---
+++ DashBoardMaster/js/stats.js
@@ -0,0 +1,529 @@
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var cbk = new ProgressBar.Line(cbknl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#20ccfe',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbk.animate(0.5);  // Number from 0.0 to 1.0
+
+var sbk = new ProgressBar.Line(sbknl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1fcafe',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbk.animate(0.6);
+
+var cbr = new ProgressBar.Line(cbrfr, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#ff7302',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbr.animate(0.2);  
+
+var cbdde = new ProgressBar.Line(cbd_de, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#5cfaad',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdde.animate(0.9); 
+
+var ipay = new ProgressBar.Line(ipay_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#d615ec',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ipay.animate(0.6);
+
+var frbecbd = new ProgressBar.Line(frbe_cbd, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1d5af0',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+frbecbd.animate(0.4); 
+
+var nlbecbd = new ProgressBar.Line(nlbe_cbd, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#5cf8af',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+nlbecbd.animate(0.5); 
+
+var cbrpl = new ProgressBar.Line(cbr_pl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f41c65',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbrpl.animate(0.6); 
+
+var cbsat = new ProgressBar.Line(cbs_at, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1ecffb',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbsat.animate(0.3);
+
+var cbdch = new ProgressBar.Line(cbd_ch, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#23caf6',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdch.animate(0.8);
+
+var ncnl = new ProgressBar.Line(nc_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#21cbff',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ncnl.animate(0.5);
+
+var ncbe = new ProgressBar.Line(nc_be, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f4c618',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ncbe.animate(0.9);
+
+var lcbnl = new ProgressBar.Line(lcb_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#c0f332',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbnl.animate(0.7);
+
+var lcbde = new ProgressBar.Line(lcb_de, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#edc71c',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbde.animate(1.0);
+
+var cbdit = new ProgressBar.Line(cbd_it, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f4c618',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdit.animate(0.5);
+
+var cbeuk = new ProgressBar.Line(cbe_uk, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#fc7406',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbeuk.animate(0.7);
+
+var cbdse = new ProgressBar.Line(cbd_se, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#5dfcac',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdse.animate(0.9);
+
+var cbdes = new ProgressBar.Line(cbd_es, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#c1f82d',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdes.animate(0.4);
+
+var lcbuk = new ProgressBar.Line(lcb_uk, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#2359ef',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbuk.animate(0.8);
+
+var lcbfr = new ProgressBar.Line(lcb_fr, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f3c420',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbfr.animate(0.8);
+
+var lcbit = new ProgressBar.Line(lcb_it, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#fe7203',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbit.animate(0.6);
+
+var lcbpl = new ProgressBar.Line(lcb_pl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f51f65',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbpl.animate(0.8);
+
+var mfnl = new ProgressBar.Line(mf_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#20ccfe',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+mfnl.animate(0.8);
+
+var csde = new ProgressBar.Line(cs_de, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#23caff',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+csde.animate(0.8);
+
+var lcbes = new ProgressBar.Line(lcb_es, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f0c715',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+lcbes.animate(0.8);
+
+var ecnl = new ProgressBar.Line(ec_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#fa7204',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ecnl.animate(0.8);
+
+var ecbe = new ProgressBar.Line(ec_be, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#24ccfd',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ecbe.animate(0.8);
+
+var cbdpt = new ProgressBar.Line(cbd_pt, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f4c618',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdpt.animate(0.8);
+
+var cbnl = new ProgressBar.Line(cb_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#df11f9',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbnl.animate(0.8);
+
+var mcnl = new ProgressBar.Line(mc_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#bff432',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+mcnl.animate(0.8);
+
+var cbdfi = new ProgressBar.Line(cbd_fi, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1fcdff',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdfi.animate(0.8);
+ 
+var ggnl = new ProgressBar.Line(gg_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#d615ec',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+ggnl.animate(0.8);
+
+var cbduk = new ProgressBar.Line(cbd_uk, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1d5af0',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbduk.animate(0.8);
+
+var cbeau = new ProgressBar.Line(cbe_au, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#5cf8af',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbeau.animate(0.8);
+
+var cbddk = new ProgressBar.Line(cbd_dk, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f51f67',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbddk.animate(0.8);
+
+var frcbdch = new ProgressBar.Line(fr_cbd_ch, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1acffe',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+frcbdch.animate(0.8);
+
+var cbdcz = new ProgressBar.Line(cbd_cz, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f4c618',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdcz.animate(0.8);
+
+var fcit = new ProgressBar.Line(fc_it, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#edc818',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+fcit.animate(0.8);
+ 
+var cbdno = new ProgressBar.Line(cbd_no, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1fcafe',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+cbdno.animate(0.8);
+ 
+var sbsnl = new ProgressBar.Line(sbs_nl, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f2c224',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsnl.animate(0.8);
+
+var sbsbe = new ProgressBar.Line(sbs_be, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f41c65',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsbe.animate(0.8);
+
+var sbsuk = new ProgressBar.Line(sbs_uk, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#d015e8',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsuk.animate(0.8);
+
+var sbsit = new ProgressBar.Line(sbs_it, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#c0f438',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsit.animate(0.8);
+
+var sbsde = new ProgressBar.Line(sbs_de, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#23caff',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsde.animate(0.8);
+
+var sbsau = new ProgressBar.Line(sbs_au, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#fb7408',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsau.animate(0.8);
+
+var sbsfr = new ProgressBar.Line(sbs_fr, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#59faae',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsfr.animate(0.8);
+
+var sbses = new ProgressBar.Line(sbs_es, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#1bd0fd',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbses.animate(0.8);
+
+var sbsse = new ProgressBar.Line(sbs_se, {
+  strokeWidth: 2,
+  easing: 'easeInOut',
+  duration: 2000,
+  color: '#f41c65',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
+});
+sbsse.animate(0.8);
